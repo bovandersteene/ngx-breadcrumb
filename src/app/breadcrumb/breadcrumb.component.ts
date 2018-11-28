01d529236ad9a04b93f4ec42e04290d519028bbe
Bo Vandersteene <bo.vandersteene@colruytgroup.com>
2018-11-28 15:59:45 +0100

    upgrade to angular 7

--- src/app/breadcrumb/breadcrumb.component.ts
+++ src/app/breadcrumb/breadcrumb.component.ts
@@ -1,45 +1,48 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { BreadCrumb } from './breadcrumb';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {BreadCrumb} from './breadcrumb';
+import {distinctUntilChanged, filter, map} from 'rxjs/operators';
 
 @Component({
-    selector: 'app-breadcrumb',
-    templateUrl: './breadcrumb.component.html',
-    styleUrls: [ './breadcrumb.component.scss' ],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BreadcrumbComponent implements OnInit {
-    breadcrumbs$ = this.router.events
-        .filter(event => event instanceof NavigationEnd)
-        .distinctUntilChanged()
-        .map(event => this.buildBreadCrumb(this.activatedRoute.root));
-    // Build your breadcrumb starting with the root route of your current activated route
+  breadcrumbs$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    distinctUntilChanged(),
+    map(event => this.buildBreadCrumb(this.activatedRoute.root))
+  );
 
-    constructor(private activatedRoute: ActivatedRoute,
-                private router: Router) {
-    }
+  // Build your breadcrumb starting with the root route of your current activated route
 
-    ngOnInit() {
-    }
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {
+  }
 
-    buildBreadCrumb(route: ActivatedRoute, url: string = '',
-                    breadcrumbs: Array<BreadCrumb> = []): Array<BreadCrumb> {
-        // If no routeConfig is avalailable we are on the root path
-        const label = route.routeConfig ? route.routeConfig.data[ 'breadcrumb' ] : 'Home';
-        const path = route.routeConfig ? route.routeConfig.path : '';
-        // In the routeConfig the complete path is not available,
-        // so we rebuild it each time
-        const nextUrl = `${url}${path}/`;
-        const breadcrumb = {
-            label: label,
-            url: nextUrl
-        };
-        const newBreadcrumbs = [ ...breadcrumbs, breadcrumb ];
-        if (route.firstChild) {
-            // If we are not on our current path yet,
-            // there will be more children to look after, to build our breadcumb
-            return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
-        }
-        return newBreadcrumbs;
+  ngOnInit() {
+  }
+
+  buildBreadCrumb(route: ActivatedRoute, url: string = '',
+                  breadcrumbs: Array<BreadCrumb> = []): Array<BreadCrumb> {
+    // If no routeConfig is avalailable we are on the root path
+    const label = route.routeConfig ? route.routeConfig.data['breadcrumb'] : 'Home';
+    const path = route.routeConfig ? route.routeConfig.path : '';
+    // In the routeConfig the complete path is not available,
+    // so we rebuild it each time
+    const nextUrl = `${url}${path}/`;
+    const breadcrumb = {
+      label: label,
+      url: nextUrl,
+    };
+    const newBreadcrumbs = [...breadcrumbs, breadcrumb];
+    if (route.firstChild) {
+      // If we are not on our current path yet,
+      // there will be more children to look after, to build our breadcumb
+      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
     }
+    return newBreadcrumbs;
+  }
 }
--- src/rx-imports.ts
+++
@@ -1,3 +0,0 @@
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/filter';
